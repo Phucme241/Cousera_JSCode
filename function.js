c313838728dbbd5f50af4015b4d0c6d504ddc5e0
Phucme241 <111069280+Phucme241@users.noreply.github.com>
2024-02-20 16:34:07 +0700

    Update function.js

--- function.js
+++ function.js
@@ -4,11 +4,17 @@ const defaultContext = "Hover over an image below to display here.";
 function upDate(previewPic){
     console.log("update");
     choosingImage.textContent = previewPic.alt;
-  
-	}
+}
 
-	function unDo(){
-     console.log("undo");
+function unDo(){
+    console.log("undo");
     choosingImage.textContent = defaultContext;
-		
-	}
+}
+
+function addTabIndexes() {
+    console.log("onload event triggered");
+    const previews = document.querySelectorAll(".preview");
+    for (let i = 0; i < previews.length; i++) {
+        previews[i].setAttribute("tabindex", "0");
+    }
+}
